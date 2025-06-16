e0234e47d1b68809e4a777767d445e64d4c81cd7
Hannah Anderson <339729345@GoTVDSB.ca>
2025-06-16 14:42:26 -0400

    Speed

--- Final Project Game/Game1.cs
+++ Final Project Game/Game1.cs
@@ -37,6 +37,22 @@ namespace Final_Project_Game
         Rectangle birdRect;
         Vector2 birdSpeed;
 
+        Texture2D pipe1UpTexture;
+        Rectangle pipe1UpRect;
+        Rectangle pipe2UpRect;
+        Rectangle pipe3UpRect;
+        Rectangle pipe4UpRect;
+        Rectangle pipe5UpRect;
+
+        Texture2D pipe1DownTexture;
+        Rectangle pipe1DownRect;
+        Rectangle pipe2DownRect;
+        Rectangle pipe3DownRect;
+        Rectangle pipe4DownRect;
+        Rectangle pipe5DownRect;
+
+        Vector2 pipeSpeed;
+
         MouseState mouseState;
         MouseState prevMouseState;
 
@@ -61,7 +77,19 @@ namespace Final_Project_Game
 
             startRect = new Rectangle(500, 350, 175, 175);
             rectangleRect = new Rectangle(523, 393, 132, 81);
-            birdRect = new Rectangle(10, 10, 75, 75);
+            birdRect = new Rectangle(5, 50, 75, 75);
+            pipe1UpRect = new Rectangle(75, 150, 65, 500);
+            pipe1DownRect = new Rectangle(75, -40, 65, 100);
+            pipe2UpRect = new Rectangle(250, 200, 65, 400);
+            pipe2DownRect = new Rectangle(250, 0, 65, 125);
+            pipe3UpRect = new Rectangle(400, 300, 65, 400);
+            pipe3DownRect = new Rectangle(400, 0, 65, 225);
+            pipe4UpRect = new Rectangle(550, 150, 65, 550);
+            pipe4DownRect = new Rectangle(550, 0, 65, 30);
+            //pipe5UpRect = new Rectangle(700, 200, 65, 400);
+            //pipe5DownRect = new Rectangle(700, 0, 65, 125);
+
+            pipeSpeed = new Vector2(-1, 0);
 
             screen = Screen.Intro;
 
@@ -81,6 +109,8 @@ namespace Final_Project_Game
             titleFont = Content.Load<SpriteFont>("Titlefont");
             birdTexture = Content.Load<Texture2D>("flappyBird");
             deadTexture = Content.Load<Texture2D>("black");
+            pipe1UpTexture = Content.Load<Texture2D>("upPipe");
+            pipe1DownTexture = Content.Load<Texture2D>("downPipe");
         }
 
         protected override void Update(GameTime gameTime)
@@ -115,6 +145,46 @@ namespace Final_Project_Game
                     birdSpeed.Y += 2;
                 }
                 birdRect.Y += (int)birdSpeed.Y;
+
+                pipe1UpRect.X += (int)pipeSpeed.X;
+                if (pipe1UpRect.Right <= 0)
+                    pipe1UpRect.X = 800;
+                pipe1DownRect.X += (int)pipeSpeed.X;
+                if (pipe1DownRect.Right <= 0)
+                    pipe1DownRect.X = 800;
+
+                pipe2UpRect.X += (int)pipeSpeed.X;
+                if (pipe2UpRect.Right <= 0)
+                    pipe2UpRect.X = 800;
+                pipe2DownRect.X += (int)pipeSpeed.X;
+                if (pipe2DownRect.Right <= 0)
+                    pipe2DownRect.X = 800;
+
+                pipe3UpRect.X += (int)pipeSpeed.X;
+                if (pipe3UpRect.Right <= 0)
+                    pipe3UpRect.X = 800;
+                pipe3DownRect.X += (int)pipeSpeed.X;
+                if (pipe3DownRect.Right <= 0)
+                    pipe3DownRect.X = 800;
+
+                pipe4UpRect.X += (int)pipeSpeed.X;
+                if (pipe4UpRect.Right <= 0)
+                    pipe4UpRect.X = 800;
+                pipe4DownRect.X += (int)pipeSpeed.X;
+                if (pipe4DownRect.Right <= 0)
+                    pipe4DownRect.X = 800;
+
+                //pipe5UpRect.X += (int)pipeSpeed.X;
+                //if (pipe5UpRect.Right <= 0)
+                //    pipe5UpRect.X = 800;
+                //pipe5DownRect.X += (int)pipeSpeed.X;
+                //if (pipe5DownRect.Right <= 0)
+                //    pipe5DownRect.X = 800;
+
+                //if (birdRect.Intersects == pipe1upTexture)
+                //    screen == Screen.Dead;
+
+
             }
 
             base.Update(gameTime);
@@ -145,6 +215,16 @@ namespace Final_Project_Game
             else if (screen == Screen.Game)
             {
                 _spriteBatch.Draw(birdTexture, birdRect, Color.White);
+                _spriteBatch.Draw(pipe1UpTexture, pipe1UpRect, Color.White);
+                _spriteBatch.Draw(pipe1DownTexture, pipe1DownRect, Color.White);
+                _spriteBatch.Draw(pipe1UpTexture, pipe2UpRect, Color.White);
+                _spriteBatch.Draw(pipe1DownTexture, pipe2DownRect, Color.White);
+                _spriteBatch.Draw(pipe1UpTexture, pipe3UpRect, Color.White);
+                _spriteBatch.Draw(pipe1DownTexture, pipe3DownRect, Color.White);
+                _spriteBatch.Draw(pipe1UpTexture, pipe4UpRect, Color.White);
+                _spriteBatch.Draw(pipe1DownTexture, pipe4DownRect, Color.White);
+                //_spriteBatch.Draw(pipe1UpTexture, pipe5UpRect, Color.White);
+                //_spriteBatch.Draw(pipe1DownTexture, pipe5DownRect, Color.White);
             }
             else if (screen == Screen.Dead)
             {
